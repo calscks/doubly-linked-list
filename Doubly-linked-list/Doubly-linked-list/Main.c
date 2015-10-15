5434519588733602909eed95d757a006e15064ff
choongfoo <koafoo516@hotmail.com>
2015-10-15 23:57:32 +0800

    fixed print from ending

--- Doubly-linked-list/Doubly-linked-list/Main.c
+++ Doubly-linked-list/Doubly-linked-list/Main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #define l 100
 
+int count = 0;
 
 typedef struct NODE
 {
@@ -36,6 +37,7 @@ void create()
 	printf("Enter customer address:\n");
 	fflush(stdin);
 	fgets(&temp->customerAdd, l, stdin);
+	count++;
 }
 
 void insert()
@@ -83,18 +85,35 @@ void display()
 	}
 	printf("\nLinked list elements from begining : \n");
 
-	while (temp2!= NULL)
+	while (temp2->next != NULL)
 	{
+		printf("Customer's name:\n");
 		printf("%s", temp2->customer);
+		printf("Customer's number:\n");
 		printf("%s", temp2->customerNum);
+		printf("Customer's gender:\n");
 		printf("%s", temp2->gender);
+		printf("Order description:\n");
 		printf("%s", temp2->orderDes);
+		printf("Customer's address:\n");
 		printf("%s", temp2->customerAdd);
 		temp2 = temp2->next;
+		system("pause");
 	}
+	printf("Customer's name:\n");
+	printf("%s", temp2->customer);
+	printf("Customer's number:\n");
+	printf("%s", temp2->customerNum);
+	printf("Customer's gender:\n");
+	printf("%s", temp2->gender);
+	printf("Order description:\n");
+	printf("%s", temp2->orderDes);
+	printf("Customer's address:\n");
+	printf("%s", temp2->customerAdd);
+	system("pause");
 }
 
-void display2(char a,char b,char c,char d,char e)
+void display2(char a[l], char b[l], char c[l], char d[l], char e[l])
 {
 	if (temp2 != NULL)
 	{
@@ -105,18 +124,83 @@ void display2(char a,char b,char c,char d,char e)
 		e = temp2->customerAdd;
 		temp2 = temp2->next;
 		display2(a,b,c,d,e);
-		printf("%s%s%s%s%s", a,b,c,d,e);
-
+		printf("Customer's name:\n");
+		printf("%s", a);
+		printf("Customer's number:\n");
+		printf("%s", b);
+		printf("Customer's gender:\n");
+		printf("%s", c);
+		printf("Order description:\n");
+		printf("%s", d);
+		printf("Customer's address:\n");
+		printf("%s", e);
+		system("pause");
 	}
+}
+
+void deleteNode()
+{
+	int i = 1, pos;
+
+	printf("\nEnter position to delete:\n");
+	scanf("%d", &pos);
+	temp2 = head;
+
+	if ((pos < 1) || (pos >= count + 1))
+	{
+		printf("\nError : Position out of range to delete");
+		return;
+	}
+	if (head == NULL)
+	{
+		printf("\nEmpty linked list.");
+		return;
+	}
+	else
+	{
+		while (i < pos)
+		{
+			temp2 = temp2->next;
+			i++;
+		}
+		if (i == 1)
+		{
+			if (temp2->next == NULL)
+			{
+				printf("Node delted from list: %d\n",pos);
+				free(temp2);
+				temp2 = head = NULL;
+				return;
+			}
+		}
+		if (temp2->next == NULL)
+		{
+			temp2->prev->next = NULL;
+			free(temp2);
+			printf("Node deleted from list: %d\n",pos);
+			return;
+		}
+		temp2->next->prev = temp2->prev;
+		if (i != 1)
+			head = temp2->next;
+		if (i = 1)
+			head = temp2->next;
+		printf("\nNode deleted");
+		free(temp2);
+	}
+	count--;
 }
 
 
 void main()
 {
+	system("color f0");
 	int choice;
 	while (1)
 	{
-		printf("1. Insert to begining\n2. Insert to ending\n3. Print\nUr choice");
+		system("cls");
+		printf("Linked list\n\n");
+		printf("1. Insert to begining\n2. Insert to ending\n3. Print from beginning\n4. Print from ending\n5. Delete from any position\n6. Quit system\nSelection: ");
 		fflush(stdin);
 		scanf("%d", &choice);
 
@@ -141,8 +225,15 @@ void main()
 				display2(temp2->customer, temp2->customerNum, temp2->gender, temp2->orderDes, temp2->customerAdd);
 			}
 			break;
-		default:
+		case 5:
+			deleteNode();
+			break;
+		case 6:
 			exit(0);
+			break;
+		default:
+			printf("\nInvalid input!!\n\a");
+			system("pause");
 			break;
 		}
 	}
